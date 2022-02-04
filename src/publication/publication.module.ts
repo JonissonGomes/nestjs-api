bbbd7ee8061ed4dbb2f0e0b5c01385ede25497cb
JonissonGomes <joni_gomes@hotmail.com>
2022-02-04 08:27:42 -0300

    Adicionando a entidade ao Module [Publication]

--- src/publication/publication.module.ts
+++ src/publication/publication.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PublicationService } from './publication.service';
 import { PublicationController } from './publication.controller';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Publication } from './entities/publication.entity';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Publication])],
   controllers: [PublicationController],
-  providers: [PublicationService]
+  providers: [PublicationService],
 })
 export class PublicationModule {}
