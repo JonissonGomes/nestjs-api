ae782bb5fac5a9a76940612b5d344a1e0f07e9c2
JonissonGomes <joni_gomes@hotmail.com>
2022-02-04 08:27:05 -0300

    Criando Entidade [Publication]

--- src/publication/entities/publication.entity.ts
+++ src/publication/entities/publication.entity.ts
@@ -1,1 +1,12 @@
-export class Publication {}
+import { Column, Table, Model } from 'sequelize-typescript';
+
+@Table({
+  tableName: 'publications',
+})
+export class Publication extends Model {
+  @Column
+  name: string;
+
+  @Column
+  description: string;
+}
