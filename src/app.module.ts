5ae8ca6a48cf37ccef00d0db4ef7fc169b0c8672
JonissonGomes <joni_gomes@hotmail.com>
2022-02-04 08:28:21 -0300

    Criando conexão com o bancode dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PublicationModule } from './publication/publication.module';
 
 @Module({
-  imports: [PublicationModule],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      host: join(__dirname, 'database.sqlite'),
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
